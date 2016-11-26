648a595ad0e35db0b94be17ea13fe80d14d6197d
Zachary-Moix <zachary.moix@gmail.com>
2016-11-25 22:41:47 -0600

    Visual improvements

--- CreateAccountScreen.java
+++ CreateAccountScreen.java
@@ -1,20 +1,24 @@
-package GUI;
+package main;
 
 import java.awt.*;
 
-import javax.swing.*; 
+import javax.swing.*;
+
+//import lab7out.AccountCreationControl; 
 
 public class CreateAccountScreen extends JFrame 
 {
-	private JTextField login, password, confirm; 
+	private JTextField login;
+	private JPasswordField password, confirm; 
 	private JButton submit, cancel; 
+	private JLabel error;
 	
 	public JTextField getLogin()
 	{
 		return login;
 	}
 	
-	public JTextField getPass()
+	public JPasswordField getPass()
 	{
 		return password;
 	}
@@ -34,36 +38,54 @@ public class CreateAccountScreen extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel grid = new JPanel(); 
 		
-		grid.setLayout(new GridLayout(4,1));
+		grid.setLayout(new GridLayout(3,2));
 		
 		JPanel passC = new JPanel();
-		confirm = new JTextField("Confirm Password");
+		confirm = new JPasswordField(10);
+		passC.setLayout(new FlowLayout(FlowLayout.LEADING));
 		passC.add(confirm);
 		
 		JPanel user = new JPanel();
-		login = new JTextField("Username"); 
-		login.setPreferredSize(confirm.getPreferredSize());
+		login = new JTextField(10); 
+		//login.setPreferredSize(confirm.getPreferredSize());\
+		user.setLayout(new FlowLayout(FlowLayout.LEADING));
 		user.add(login);
 		
 		JPanel pass = new JPanel();
-		password = new JTextField("Password");
-		password.setPreferredSize(confirm.getPreferredSize());
+		password = new JPasswordField(10);
+		//password.setPreferredSize(confirm.getPreferredSize());
+		pass.setLayout(new FlowLayout(FlowLayout.LEADING));
 		pass.add(password);
 		
-		
+		JLabel ul = new JLabel("Username: ");
+		JPanel u = new JPanel();
+		u.add(ul);
+		u.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		JLabel pl = new JLabel("Password: ");
+		JPanel p = new JPanel();
+		p.add(pl);
+		p.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		JLabel cl = new JLabel("Confirm Password: ");
+		JPanel c = new JPanel();
+		c.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		c.add(cl);
 		JPanel buttons = new JPanel();
 		submit = new JButton("Submit");
 		cancel = new JButton("Cancel");
 		buttons.add(submit);
 		buttons.add(cancel);
 		
+		grid.add(u);
 		grid.add(user);
+		grid.add(p);
 		grid.add(pass);
+		grid.add(c);
 		grid.add(passC);
-		grid.add(buttons);
+		this.add(buttons, BorderLayout.SOUTH);
 		
 		this.add(grid, BorderLayout.CENTER);
 		this.setVisible(true);
+		//this.setSize(400,300);
 		this.pack();
 	}
 	
--- LoginScreen.java
+++ LoginScreen.java
@@ -1,4 +1,4 @@
-package GUI;
+package main;
 
 import java.awt.*;
 
@@ -6,14 +6,15 @@ import javax.swing.*;
 
 public class LoginScreen extends JFrame 
 {
-	private JTextField login, password; 
+	private JTextField login;
+	private JPasswordField password; 
 	private JButton submit, cancel; 
 	
 	public JTextField getLogin()
 	{
 		return login;
 	}
-	public JTextField getPass()
+	public JPasswordField getPass()
 	{
 		return password;
 	}
@@ -30,28 +31,45 @@ public class LoginScreen extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel grid = new JPanel(); 
 		
-		grid.setLayout(new GridLayout(3,1));
+		grid.setLayout(new GridLayout(2,2));
+		
 		
 		JPanel user = new JPanel();
-		login = new JTextField("Username"); 
+		login = new JTextField(10); 
+		//login.setPreferredSize(confirm.getPreferredSize());\
+		user.setLayout(new FlowLayout(FlowLayout.LEADING));
 		user.add(login);
 		
 		JPanel pass = new JPanel();
-		password = new JTextField("Password");
+		password = new JPasswordField(10);
+		//password.setPreferredSize(confirm.getPreferredSize());
+		pass.setLayout(new FlowLayout(FlowLayout.LEADING));
 		pass.add(password);
 		
+		JLabel ul = new JLabel("Username: ");
+		JPanel u = new JPanel();
+		u.add(ul);
+		u.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		JLabel pl = new JLabel("Password: ");
+		JPanel p = new JPanel();
+		p.add(pl);
+		p.setLayout(new FlowLayout(FlowLayout.TRAILING));
 		JPanel buttons = new JPanel();
 		submit = new JButton("Submit");
 		cancel = new JButton("Cancel");
 		buttons.add(submit);
 		buttons.add(cancel);
 		
+		grid.add(u);
 		grid.add(user);
+		grid.add(p);
 		grid.add(pass);
-		grid.add(buttons);
+
+		this.add(buttons, BorderLayout.SOUTH);
 		
 		this.add(grid, BorderLayout.CENTER);
 		this.setVisible(true);
+		//this.setSize(400,300);
 		this.pack();
 	}
 	
